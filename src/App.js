92c3abaf885e57422277e2f40c7c03a2dba941b1
Shannon Clarke <shannonajclarke@gmail.com>
2018-10-01 08:38:53 -0400

    Add Polymath branding

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Web3 from 'web3'
+
+import logo from './poly_logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      account:null
+    }
+  }
+
+  componentDidMount = async() => {
+    try{
+
+      const web3 = new Web3(Web3.givenProvider)
+      const accounts = await web3.eth.getAccounts()
+
+      this.setState({account:accounts[0]})
+
+    }catch(error){
+      console.log("Could not connect to web3",error)
+    }
+  }
+
   render() {
+
+    const {account} = this.state
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={logo} alt="logo" />
+          <h1 className="App-title">Polymath Tutorial</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+
+        {(!account)?(
+          <span>Loading...</span>
+        ):(
+          <span>Your ETH address is {this.state.account}</span>
+        )}
+          
         </p>
       </div>
     );
